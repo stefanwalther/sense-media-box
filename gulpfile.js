eec84bcdaa44a95f6d8bbed0bae7b359adfec44b
Stefan Walther <Stefan.Walther@qlik.com>
2016-05-20 20:26:58 +0200

    Simplify gulp-file (sense-go 0.6)

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
+'use strict';
 var gulp = require('gulp');
 var senseGo = require('sense-go');
 
-var userConfig = {
-	"packageName": "sense-media-box"
-};
-
-senseGo.init( gulp, userConfig,  function (  ) {
-	// Your own gulp tasks or task-chains here
-	// ...
+senseGo.init( gulp, function (  ) {
+	
 });
