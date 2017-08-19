fc8b214004863bc27fd8024efbaccf1649ae9c5f
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-08-19 23:47:01 +0200

    Bump v0.4.3

--- build/dev/lib/directives/qsMbVideo/qsMbVideo.js
+++ build/dev/lib/directives/qsMbVideo/qsMbVideo.js
@@ -2,7 +2,7 @@
 
 * sense-media-box - Include web pages, videos, images and much more into your Qlik Sense app.
 * --
-* @version v0.4.2
+* @version v0.4.3
 * @link https://github.com/stefanwalther/sense-media-box
 * @author Stefan Walther
 * @license MIT
